455789e4798165ddc1d775bfd4eb16802ac525e7
MJC-macbook <mjchoi@nfri.re.kr>
2018-08-09 19:01:39 +0900

    test B_abs in geqdsk_dk

--- eq_profiles.py
+++ eq_profiles.py
@@ -1,2 +1,9 @@
 # otbain 2D profiles
-from geqdsk_dk import geqdsk_dk as geqdsk
+import geqdsk_dk
+geqdsk_fn = "../data/g013728.003900"
+g1 = geqdsk_dk(filename=geqdsk_fn)
+R = 2.0
+z = 0.3
+
+B = g1.B_abs(R,z)
+print(B)
