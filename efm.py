1cb4d422e2dc790f2ac6630f22aa7e20fd9c6ac1
MJC-macbook <mjchoi@nfri.re.kr>
2018-08-17 17:46:46 +0900

    minor

--- efm.py
+++ efm.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 from kei import *
 from pfunc import *
 from bpath import torbeam_prof, write_inbeam, run_torbeam, set_beam_path, vac_beam_path
-from eceint import ece_intensity
 from pintp import intp_prof
+from eceint import ece_intensity
 
 from time import strftime
 
